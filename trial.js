efe4720a238365ef0aa71999833d788144323959
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-28 23:59:31 +0300

    inside HTML

--- trial.js
+++ trial.js
@@ -1,7 +1,13 @@
-let myTitle = document.getElementById("head").innerHTML
-console.log(myTitle)
-let myTitle2 = document.getElementById("head").firstChild.nodeValue
-console.log(myTitle2)
-console.log(document.getElementById("head"))
-let myTitle3 = document.getElementById("head").childNodes[0].nodeValue
-console.log(myTitle3)
+// let myTitle = document.getElementById("head").innerHTML
+// console.log(myTitle)
+// let myTitle2 = document.getElementById("head").firstChild.nodeValue
+// console.log(myTitle2)
+// console.log(document.getElementById("head"))
+// let myTitle3 = document.getElementById("head").childNodes[0].nodeValue
+// console.log(myTitle3)
+
+
+console.log(document.head.innerHTML)
+console.log(document.body.innerHTML)
+console.log(document.documentElement.innerHTML)
+console.log(document.getElementById("head").nodeName)
