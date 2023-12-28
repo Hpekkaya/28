deb1eeb1da0213b11063e152be2c49a5b9233537
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-28 23:57:31 +0300

    nodes tried

--- trial.js
+++ trial.js
@@ -1,19 +1,7 @@
-debugger;
-function ort(a, b, c) {
-  debugger
-  a = a + 5;
-  b = b + 5;
-  c = c + 5;
-  return (a + b + c) / 3;
-}
-let a = 10;
-let b = 110;
-let c = 120;
-let d = 130;
-let e = 140;
-
-let x = ort(75, 80, 85);
-a = b - a;
-b = c + d;
-d = b * a;
-console.log(x);
+let myTitle = document.getElementById("head").innerHTML
+console.log(myTitle)
+let myTitle2 = document.getElementById("head").firstChild.nodeValue
+console.log(myTitle2)
+console.log(document.getElementById("head"))
+let myTitle3 = document.getElementById("head").childNodes[0].nodeValue
+console.log(myTitle3)
