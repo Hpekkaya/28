a64340da2e474beffd14b8020ab7a10abc60c367
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-28 21:03:55 +0300

    breakpoint tried

---
+++ trial.js
@@ -0,0 +1,19 @@
+debugger;
+function ort(a, b, c) {
+  debugger
+  a = a + 5;
+  b = b + 5;
+  c = c + 5;
+  return (a + b + c) / 3;
+}
+let a = 10;
+let b = 110;
+let c = 120;
+let d = 130;
+let e = 140;
+
+let x = ort(75, 80, 85);
+a = b - a;
+b = c + d;
+d = b * a;
+console.log(x);
