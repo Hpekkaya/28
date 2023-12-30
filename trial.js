80d3a8d521ae772121314acc55128ec9c6202d6b
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-30 09:52:44 +0300

    http request tried

--- trial.js
+++ trial.js
@@ -1,24 +1,12 @@
-// /storage api 
-// localStorage.setItem("name2","Hakan Pekkaya")
-// const item = localStorage.getItem("name")
-// console.log(item)
+// ajax (Asyncronous JavaScript and XML)
 
-// Problem1 : Local_storage : save as string
-// // Problem2 : Local_storage : save on it after delete
-
-// const name  = ["Ahmet", "Mustafa"]
-// localStorage.setItem("name", JSON.stringify(name))
-// const item2 = JSON.parse(localStorage.getItem("name"))
-// console.log(item2)
-
-const name1  = ["Mehmet", "Muhammed"]
-localStorage.setItem("name", JSON.stringify(name1))
-const item = JSON.parse(localStorage.getItem("name"))
-console.log(item)
-const name2 =  [...JSON.parse(localStorage.getItem("name")),"Mahmut","Ali"]
-localStorage.setItem("name",JSON.stringify(name2))
-console.log(JSON.parse(localStorage.getItem("name")))
-
-localStorage.removeItem("name")
-
-// In order to add new
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function(){
+    debugger
+    // onload what to do
+    document.getElementById("demo").innerHTML = this.responseText
+    console.log(this)
+}
+debugger
+xhttp.open("GET","ajax.txt")
+xhttp.send()
