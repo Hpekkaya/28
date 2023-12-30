cdaa11777f40ef5260b420d4a2267111428d399f
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-30 11:37:11 +0300

    XML Httprequest repeated

--- trial.js
+++ trial.js
@@ -1,15 +1,27 @@
 // ajax (Asyncronous JavaScript and XML)
 
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function(){
+// const xhttp = new XMLHttpRequest();
+// xhttp.onload = function(){
     
-    // onload what to do
-    document.getElementById("demo").innerHTML = JSON.parse(this.responseText).age
-    console.log(JSON.parse(this.responseText))
+//     // onload what to do
+//     document.getElementById("demo").innerHTML = JSON.parse(this.responseText).age
+//     console.log(JSON.parse(this.responseText))
+// }
+
+// xhttp.open("GET","ajax.json")
+// xhttp.send()
+
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    console.log(this.readyState)
+    console.log(this.status)
 }
 
-xhttp.open("GET","ajax.json")
+xhttp.onload = function(){
+    document.getElementById("demo").innerHTML=this.responseText;
+    console.log(this)
+}
+xhttp.open("GET","ajax.txt")
 xhttp.send()
-
 
 
