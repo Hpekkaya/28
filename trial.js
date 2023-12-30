4a1c4416b180f2bf2e3268ab515a058d34a40e9d
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-30 10:04:35 +0300

    JSON parse

--- trial.js
+++ trial.js
@@ -2,11 +2,14 @@
 
 const xhttp = new XMLHttpRequest();
 xhttp.onload = function(){
-    debugger
+    
     // onload what to do
-    document.getElementById("demo").innerHTML = this.responseText
-    console.log(this)
+    document.getElementById("demo").innerHTML = JSON.parse(this.responseText).age
+    console.log(JSON.parse(this.responseText))
 }
-debugger
-xhttp.open("GET","ajax.txt")
+
+xhttp.open("GET","ajax.json")
 xhttp.send()
+
+
+
